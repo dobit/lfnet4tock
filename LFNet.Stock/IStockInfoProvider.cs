e4d5c8106e485fb59539de356ba5b07d6d20365b
dobit <dobit@163.com>
2014-11-24 11:13:14 +0800

    初始化项目

---
+++ LFNet.Stock/IStockInfoProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace LFNet.Stock
+{
+    /// <summary>
+    /// 股票信息获取
+    /// </summary>
+    public interface IStockInfoProvider
+    {
+        StockInfo GetStockInfoByName(string stockName);
+
+        StockInfo GetStockInfoByCode(string stockCode);
+    }
+}
---
+++ LFNet.Stock/StockInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFNet.Stock
+{
+    /// <summary>
+    /// 股票信息
+    /// </summary>
+    public class StockInfo
+    {
+        /// <summary>
+        /// 股票名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 代码
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
